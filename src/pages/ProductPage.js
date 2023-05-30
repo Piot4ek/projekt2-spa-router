a84c67ffec53ce141763e96e9453096cf2b1c5d3
Piotr Biernacki <piot4ek@gmail.com>
2023-05-30 13:28:47 +0200

    v.1.0

---
+++ src/pages/ProductPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import Product from "../components/Product";
+
+const ProductPage = (props) => {
+  const { id } = useParams();
+  //   console.log(props);
+  return (
+    <>
+      <div>Strona produktu {id}</div>
+      <Product id={id} />
+      <Link to="/products">Powrót do listy produktów</Link>
+    </>
+  );
+};
+
+export default ProductPage;
